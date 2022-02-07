4b84906beee4d31372649683c1b1470acd83698d
Oleksandr Soloviov <shakal194@gmail.com>
2022-02-07 15:14:25 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
 const listRef = document.querySelector("#ingredients");
-const listMark = ingredients
-  .map((ingredient) => `<li class="item">${ingredient}</li>`)
-  .join("");
 
-listRef.innerHTML = listMark;
+ingredients.forEach(function (ingredient) {
+  const itemRef = document.createElement("li");
+  itemRef.classList.add = "item";
+  itemRef.textContent = ingredient;
+  listRef.append(itemRef);
+  console.log(itemRef);
+});
+
+// const listMark = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
+
+// listRef.innerHTML = listMark;
