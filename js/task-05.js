1e79a6b22394ac9bb4c2c22cf66189c94e32589d
Oleksandr Soloviov <shakal194@gmail.com>
2022-02-07 20:17:33 +0200

    update

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,10 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
+console.log(textInput);
+
 textInput.addEventListener("input", (event) => {
+  console.log(event);
   if (event.currentTarget.value === "") {
     output.textContent = "Anonymous";
   } else {
--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length === textInput.dataset.length) {
+  if (event.currentTarget.value.length == textInput.dataset.length) {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
   } else {
